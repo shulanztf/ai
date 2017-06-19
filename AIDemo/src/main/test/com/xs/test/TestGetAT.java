c631da4b6c5de6e089fee81a7cbb4b56d717948d
小帅丶 <578274480@qq.com>
2017-06-19 11:06:55 +0800

    :sparkles: 新加Java第三方库生成CA证书的工具类。个人方便查阅而已。没有其他用途。

--- AIDemo/src/main/test/com/xs/test/TestGetAT.java
+++ AIDemo/src/main/test/com/xs/test/TestGetAT.java
@@ -3,11 +3,14 @@ package com.xs.test;
 import com.xs.common.APIContants;
 import com.xs.pojo.BDAccessToken;
 import com.xs.util.baidu.AIUtil;
+import com.xs.util.baidu.HttpUtil;
 
 public class TestGetAT {
 	public static void main(String[] args) throws Exception {
-		AIUtil aiUtil = new AIUtil();
-		BDAccessToken accessToken =  aiUtil.getAccessToken(APIContants.NLP_API_KEY, APIContants.NLP_SERCET_KEY);
-		System.out.println(accessToken.getAccess_token());
+		HttpUtil httpUtil = new  HttpUtil();
+		String requestUrl = "https://www.xsshome.cn/test/testCon/test";
+		String params ="test";
+		String str = httpUtil.post(requestUrl, params);
+		System.out.println(str);
 	}
 }
