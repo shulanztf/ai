c993c06500de4deb1f721e7efb5200369a3410cd
小帅丶 <578274480@qq.com>
2017-06-21 09:30:20 +0800

    :sparkles: 封装Cert相关常用方法eg:申请证书。获取信任证书

---
+++ AIDemo/src/main/java/com/xs/demo/util/Cert.java
@@ -0,0 +1,272 @@
+package com.xs.demo.util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.security.auth.x500.X500Principal;
+
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
+import org.bouncycastle.x509.extension.AuthorityKeyIdentifierStructure;
+import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
+
+import com.xiaoleilu.hutool.lang.Base64;
+/**
+ * 使用X509 V3版本生成数字证书
+ * @author 小帅丶
+ * @date 2017年6月21日
+ */
+public class Cert {
+	/**
+	 * 根证书路径
+	 */
+	private static final String CA_PATH="G:/cav3/root/root.cer";
+	/**
+	 * 私钥keystore路径
+	 */
+	private static final String CA_PRIKEY_PATH="G:/cav3/root/privateKey.keystore";
+	/**
+	 * 默认签名算法
+	 */
+	private static final String SIGNATURE_ALGORITHM="SHA1withRSA";
+	/**
+	 * 申请数字证书
+	 * @param subjectDN 证书使用者等相关信息
+	 * @param publicKey 需要签名的公钥 base64编码
+	 * @return cert的base64编码
+	 * @throws Exception
+	 */
+	public static String AppCertReq(String subjectDN,String publicKey){
+		//接收证书的字符串
+		StringBuffer certStr = new StringBuffer();
+		try {
+		//生成X509 V3版本证书的类
+		X509V3CertificateGenerator certGen = null;
+		//接收证书的类
+		X509Certificate subCert = null;
+		//ROOT证书
+		X509Certificate rootCA = rootCA(CA_PATH);
+		//ROOT的私钥
+		PrivateKey privateKey = getpriKey(CA_PRIKEY_PATH); 
+		//默认使用BC做机构
+		Security.addProvider(new BouncyCastleProvider());
+		//证书的序列号
+		BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());
+		//颁发者信息通过CA证书获取
+		X500Principal issuerDn = rootCA.getIssuerX500Principal();
+		//使用者信息
+		X500Principal subjectDn = new X500Principal(subjectDN);
+		//生效日期 默认立即生效
+		Date notBefore = new Date();
+		//截止日期 默认5年
+		Date notAfter  = getYearLater(5);
+		//需要签名的公钥
+		PublicKey pubKey = loadPublicKeyByStr(publicKey);
+		certGen = new X509V3CertificateGenerator();
+		certGen.setPublicKey(pubKey);
+		certGen.setSerialNumber(serialNumber);
+		certGen.setIssuerDN(issuerDn);
+		certGen.setNotBefore(notBefore);
+		certGen.setNotAfter(notAfter);
+		certGen.setSubjectDN(subjectDn);
+		certGen.setSignatureAlgorithm(SIGNATURE_ALGORITHM);
+		//添加主体密钥(公钥)
+		certGen.addExtension(X509Extensions.SubjectKeyIdentifier, false,
+					new SubjectKeyIdentifierStructure(pubKey));
+	   //添加颁发者密钥(公钥)
+		certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false,
+				new AuthorityKeyIdentifierStructure(rootCA.getPublicKey()));
+		//生成X509证书
+		subCert = certGen.generate(privateKey,"BC");
+		//拼接成BASE64编码
+		certStr.append("-----BEGIN CERTIFICATE-----\n");
+		certStr.append(Base64.encode(subCert.getEncoded()));
+		certStr.append("\n-----END CERTIFICATE-----\n");
+		//保存证书
+		saveCert(subCert);
+//		catch (CertificateParsingException e) {
+//			logger.info("证书解析异常"+e.getMessage());
+//		} catch (InvalidKeyException e) {
+//			logger.info("无效的Key 请检查"+e.getMessage());
+//		} catch (CertificateEncodingException e) {
+//			logger.info("证书编码异常"+e.getMessage());
+//		} catch (IllegalStateException e) {
+//			logger.info("非正常状态"+e.getMessage());
+//		} catch (NoSuchProviderException e) {
+//			logger.info(" 供应商不存在 "+e.getMessage());
+//		} catch (NoSuchAlgorithmException e) {
+//			logger.info("算法不存在"+e.getMessage());
+//		}catch (SignatureException e) {
+//			logger.info("签名错误"+e.getMessage());
+//		} catch (FileNotFoundException e) {
+//			logger.info("文件找不到"+e.getMessage());
+//		}
+		}  catch (Exception e) {
+			certStr.setLength(0);
+			certStr.append("系统错误,请稍后再试");
+		} 
+		return certStr.toString();
+	}
+	/**
+	 * 保存申请的证书到指定盘符
+	 * @param cert
+	 * @throws Exception
+	 */
+	public static void saveCert(X509Certificate cert) throws Exception{
+		String address = "G:/CERT/"+new Date().getTime()/1000+".cer";
+		FileOutputStream outputStream = new FileOutputStream(address);
+		Writer writer = new OutputStreamWriter(outputStream,Charset.forName("UTF-8"));
+		writer.write("-----BEGIN CERTIFICATE-----\n");
+		writer.write(new sun.misc.BASE64Encoder().encode(cert.getEncoded()));
+		writer.write("\n-----END CERTIFICATE-----\n");
+		writer.flush();
+	}
+	/**
+	 * 获取根证书相关信息
+	 * @param rootPath
+	 * @return cert
+	 * @throws Exception
+	 */
+	public static X509Certificate rootCA(String rootPath) throws Exception{
+		X509Certificate cert = null;
+		FileInputStream fis = new FileInputStream(rootPath);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		while (bis.available() > 0) {
+			cert = (X509Certificate) cf.generateCertificate(fis);
+		}
+		return cert;
+	}
+	/**
+	 * 计算当前时间的N年后
+	 * @param later 正整数
+	 * @return
+	 */
+	public static Date getYearLater(int later) {
+		Date date = new Date();
+		try {
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.YEAR,later);
+			date = calendar.getTime();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return date;
+	}
+	/**
+	 * 从字符串中读取公钥
+	 * @param publicKeyStr 字符串
+	 * @return RSAPublicKey ras公钥对象
+	 * @throws Exception
+	 */
+	public static RSAPublicKey loadPublicKeyByStr(String publicKeyStr)
+			throws Exception {
+		byte[] buffer = Base64.decode(publicKeyStr);
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+			return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("算法不存在");
+		} catch (InvalidKeySpecException e) {
+			throw new Exception("公钥非法");
+		}
+	}
+	/**
+	 * 根据路径获取公钥对象
+	 * @param path
+	 * @return
+	 * @throws Exception
+	 */
+	public static PublicKey getpubKey(String path) throws Exception{
+		StringBuffer str = new StringBuffer("");
+		File file = new File(path);
+		X509EncodedKeySpec keySpec = null;
+		KeyFactory keyFactory = null;
+		try {
+			FileReader fileReader = new FileReader(file);
+			int ch = 0;
+			while ((ch=fileReader.read())!=-1) {
+				str.append((char)ch);
+			}
+			fileReader.close();
+			byte[] buffer = Base64.decode(str.toString());
+			keyFactory = KeyFactory.getInstance("RSA");
+			keySpec = new X509EncodedKeySpec(buffer);
+			return keyFactory.generatePublic(keySpec);
+		} catch (Exception e) {
+			throw new Exception("公钥数据为空");
+		}
+	}
+	/**
+	 * 根据路径获取公钥字符串
+	 * @param path
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getpubKeystr(String path) throws Exception{
+		StringBuffer str = new StringBuffer("");
+		File file = new File(path);
+		X509EncodedKeySpec keySpec = null;
+		KeyFactory keyFactory = null;
+		try {
+			FileReader fileReader = new FileReader(file);
+			int ch = 0;
+			while ((ch=fileReader.read())!=-1) {
+				str.append((char)ch);
+			}
+			fileReader.close();
+		} catch (Exception e) {
+			throw new Exception("公钥数据为空");
+		}
+		return str.toString();
+	}
+	/**
+	 * 根据路径获取私钥对象
+	 * @param path
+	 * @return
+	 * @throws Exception
+	 */
+	public static PrivateKey getpriKey(String path) throws Exception{
+		StringBuffer str = new StringBuffer("");
+		File file = new File(path);
+		PKCS8EncodedKeySpec keySpec = null;
+		KeyFactory keyFactory = null;
+		try {
+			FileReader fileReader = new FileReader(file);
+			int ch = 0;
+			while ((ch=fileReader.read())!=-1) {
+				str.append((char)ch);
+			}
+			fileReader.close();
+			byte[] buffer = Base64.decode(str.toString());
+			keyFactory = KeyFactory.getInstance("RSA");
+			keySpec = new PKCS8EncodedKeySpec(buffer);
+			return keyFactory.generatePrivate(keySpec);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new Exception("私钥数据为空");
+		}
+	}
+}
