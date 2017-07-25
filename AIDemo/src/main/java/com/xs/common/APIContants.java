dd77b7131221e726ed48a9d68b3faaa98e85933f
小帅丶 <578274480@qq.com>
2017-07-25 14:42:37 +0800

    :memo:删除自己的相关应用信息。如果使用请替换成自己的相关信息

--- AIDemo/src/main/java/com/xs/common/APIContants.java
+++ AIDemo/src/main/java/com/xs/common/APIContants.java
@@ -6,36 +6,38 @@ package com.xs.common;
  */
 public class APIContants {
 	//百度人脸识别应用id
-	public static final String APPID = "9400741";
+	public static final String APPID = "替换成自己相应的值";
 	//百度人脸识别应用apikey
-	public static final String API_KEY = "QvsxPu7uMhaGRr63ksbjhwPV";
+	public static final String API_KEY = "替换成自己相应的值";
 	//百度人脸识别应用sercetkey
-	public static final String SERCET_KEY = "fWMvVdXR4rhWizlLEnzciBSIbConuUDj";
+	public static final String SERCET_KEY = "替换成自己相应的值";
 	//百度人脸识别token 有效期一个月
-	public static final String TOKEN = "24.67b868167c4166edf2b8ba84aea87ce2.2592000.1495347925.282335-9400741";
+	public static final String TOKEN = "根据APIKEY & SERCERKEY生成ACCESSTOKEN";
 	
 	//百度文字识别应用id
-	public static final String IDCARD_APPID = "9429648";
+	public static final String IDCARD_APPID = "替换成自己相应的值";
 	//百度文字识别应用apikey
-	public static final String IDCARD_API_KEY = "l4bfjhVDEGI0LwGYxP8QmOZB";
+	public static final String IDCARD_API_KEY = "替换成自己相应的值";
 	//百度文字识别应用sercetkey
-	public static final String IDCARD_SERCET_KEY = "gD7BLIW0YZYN7VOy5f7x7hF1H4aMh0Gn";
+	public static final String IDCARD_SERCET_KEY = "替换成自己相应的值";
 	//百度文字识别token 有效期一个月
-	public static final String IDCARD_TOKEN = "24.b69d35031a118c6476918f4740aff139.2592000.1496548888.282335-9429648";
+	public static final String IDCARD_TOKEN = "根据APIKEY & SERCERKEY生成ACCESSTOKEN";
 	
 	//百度语音应用id
-	public static final String VOICE_APPID = "9695383";
+	public static final String VOICE_APPID = "替换成自己相应的值";
 	//百度语音应用apikey
-	public static final String VOICE_API_KEY = "86katcUA6rgo85UsRSfXitPR";
+	public static final String VOICE_API_KEY = "替换成自己相应的值";
 	//百度语音应用sercetkey
-	public static final String VOICE_SERCET_KEY = "SsjgHrrSGaOCOIBrfyxgglTXinLwKYVF";
-	public static final String VOICE_TOKEN = "24.abc848fabd207417ad4e09fc6137faa0.2592000.1498360048.282335-9695383";
+	public static final String VOICE_SERCET_KEY = "替换成自己相应的值";
+	public static final String VOICE_TOKEN = "根据APIKEY & SERCERKEY生成ACCESSTOKEN";
 
 	//百度语音应用id
-	public static final String NLP_APPID = "9748117";
+	public static final String NLP_APPID = "替换成自己相应的值";
 	//百度语音应用apikey
-	public static final String NLP_API_KEY = "bdKTyS2sFwKzYaEbxNKwmLpM";
+	public static final String NLP_API_KEY = "替换成自己相应的值";
 	//百度语音应用sercetkey
-	public static final String NLP_SERCET_KEY = "PPYv4XoBTluaV1HX16B5TFLPW8GUhNyw";
-	public static final String NLP_TOKEN = "24.96286c48d32a49cbe4e36bf384d1e4dc.2592000.1499587335.282335-9748117";
+	public static final String NLP_SERCET_KEY = "替换成自己相应的值";
+	public static final String NLP_TOKEN = "根据APIKEY & SERCERKEY生成ACCESSTOKEN";
+	
+	
 }
