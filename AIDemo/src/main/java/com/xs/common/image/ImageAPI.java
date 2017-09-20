84bbe950e766d7ac69f4c5ef83b2a692234d182d
小帅丶 <578274480@qq.com>
2017-09-20 14:52:05 +0800

    :sparkles:整理图像识别的接口URL

---
+++ AIDemo/src/main/java/com/xs/common/image/ImageAPI.java
@@ -0,0 +1,23 @@
+package com.xs.common.image;
+/**
+ * 图像识别API地址类
+ * @author 小帅丶
+ */
+public class ImageAPI {
+	/**
+	 * 菜品识别
+	 */
+	public static String DISH_API = "https://aip.baidubce.com/rest/2.0/image-classify/v2/dish";
+	/**
+	 * 车型识别
+	 */
+	public static String CAR_API = "https://aip.baidubce.com/rest/2.0/image-classify/v1/car";
+	/**
+	 * LOGO识别
+	 */
+	public static String LOGO_API = "https://aip.baidubce.com/rest/2.0/image-classify/v2/logo";
+	/**
+	 * 图像主体识别
+	 */
+	public static String OBJECT_DETECT_API = "https://aip.baidubce.com/rest/2.0/image-classify/v1/object_detect";
+}
