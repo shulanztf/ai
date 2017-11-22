a18e1020c7011aff933c9e75d61eae03180e8b7e
小帅丶 <578274480@qq.com>
2017-11-22 11:29:22 +0800

    :sparkles:新增腾讯AI 人脸检测Java-API形式示例代码

---
+++ AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
@@ -0,0 +1,12 @@
+package com.xs.tencent;
+
+public class TencentAPI {
+	//自己的APPID
+	public static final Integer APP_ID_AI = 0;
+	//自己的APPKEY
+	public static final String APP_KEY_AI = "控制台获取";
+	/**
+	 * 人脸检测接口地址
+	 */
+	public static final String FACE_DETECT  ="https://api.ai.qq.com/fcgi-bin/face/face_detectface";
+}
---
+++ AIDemo/src/main/java/com/xs/tencent/face/FaceDetectTest.java
@@ -0,0 +1,44 @@
+package com.xs.tencent.face;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import com.xs.tencent.HttpsUtil4Tencent;
+import com.xs.tencent.TencentAPI;
+import com.xs.tencent.sign.TencentAISign;
+import com.xs.util.baidu.Base64Util;
+import com.xs.util.baidu.FileUtil;
+import com.xs.util.baidu.HttpUtils;
+/**
+ * 腾讯AI人脸检测示例Demo
+ * @author 小帅
+ *
+ */
+public class FaceDetectTest {
+	public static void main(String[] args) throws Exception {
+		//时间戳
+		String time_stamp = System.currentTimeMillis()/1000+"";
+		//图片的二进制数组数据
+		byte [] imageData = FileUtil.readFileByBytes("G:/test2.jpg");
+		//图片的base64编码数据
+		String img64 = Base64Util.encode(imageData);
+		//随机字符串
+		String nonce_str = TencentAISign.getRandomString(10);
+		//计算SIGN
+		String sign = TencentAISign.appSignAI4FaceDetect(TencentAPI.APP_ID_AI, nonce_str,img64,1);
+		Map<String, String> headers = new HashMap<String, String>();
+		Map<String, String> bodys = new HashMap<String, String>();
+		headers.put("Content-Type", "application/x-www-form-urlencoded");
+		bodys.put("app_id","1106471787");
+		bodys.put("time_stamp",time_stamp);
+		bodys.put("nonce_str", nonce_str);
+		bodys.put("sign", sign);
+		bodys.put("image", img64);
+		bodys.put("mode", "1");
+		HttpResponse responseBD = HttpsUtil4Tencent.doPostTencentAI("https://api.ai.qq.com/fcgi-bin/face/face_detectface", headers, bodys);
+		System.out.println(EntityUtils.toString(responseBD.getEntity()));
+	}
+}
