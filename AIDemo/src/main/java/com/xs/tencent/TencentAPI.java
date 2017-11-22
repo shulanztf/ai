df91bd7ac239ee5b10db08b01648730c443da39c
小帅丶 <578274480@qq.com>
2017-11-22 14:09:30 +0800

    :sparkles:增加人脸变装  美妆接口地址

--- AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
+++ AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
@@ -2,11 +2,19 @@ package com.xs.tencent;
 
 public class TencentAPI {
 	//自己的APPID
-	public static final Integer APP_ID_AI = 0;
+	public static final Integer APP_ID_AI = 1106471787;
 	//自己的APPKEY
-	public static final String APP_KEY_AI = "控制台获取";
+	public static final String APP_KEY_AI = "tWCXFJfrSVEmu0IB";
 	/**
 	 * 人脸检测接口地址
 	 */
-	public static final String FACE_DETECT  ="https://api.ai.qq.com/fcgi-bin/face/face_detectface";
+	public static final String FACE_DETECT = "https://api.ai.qq.com/fcgi-bin/face/face_detectface";
+	/**
+	 * 人脸美妆接口地址
+	 */
+	public static final String FACE_COSMETIC = "https://api.ai.qq.com/fcgi-bin/face/face_detectface";
+	/**
+	 * 人脸变装
+	 */
+	public static final String FACE_DECORATION = "https://api.ai.qq.com/fcgi-bin/ptu/ptu_facedecoration";
 }
