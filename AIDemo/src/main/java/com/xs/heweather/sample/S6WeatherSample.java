3c252ae057eab9d7f4451ceff925f2953b604683
小帅帅丶 <578274480@qq.com>
2017-12-01 10:26:33 +0800

    更新 S6WeatherSample.java

--- AIDemo/src/main/java/com/xs/heweather/sample/S6WeatherSample.java
+++ AIDemo/src/main/java/com/xs/heweather/sample/S6WeatherSample.java
@@ -8,11 +8,13 @@ import com.xs.heweather.util.HttpUtilForHeWeather;
 
 /**
  * 和风S6接口SIGN示例代码
+ * https://free-api.heweather.com/s6/weather/now?parameters
+ * 实况天气示例代码(非官方)
  * @author 小帅丶
  *
  */
 public class S6WeatherSample {
-	public static String username="自己的USERID在和风控制台看";
+	public static String username="自己的用户ID在和风控制台看";
 	public static void main(String[] args) throws Exception {
 		String t = String.valueOf(new Date().getTime()/1000);
 		System.out.println(t);
