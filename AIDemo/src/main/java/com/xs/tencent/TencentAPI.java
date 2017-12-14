f790ec1ab8b5f6e0e6cd532cf00a4cf9f124f897
小帅丶 <578274480@qq.com>
2017-12-14 18:04:53 +0800

    :sparkles:增加语音识别-echo版

--- AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
+++ AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
@@ -2,9 +2,9 @@ package com.xs.tencent;
 
 public class TencentAPI {
 	//自己的APPID
-	public static final Integer APP_ID_AI = 12346;
+	public static final Integer APP_ID_AI = 123456;
 	//自己的APPKEY
-	public static final String APP_KEY_AI = "123456";
+	public static final String APP_KEY_AI = "自己的key";
 	/**
 	 * 人脸检测接口地址
 	 */
@@ -24,4 +24,6 @@ public class TencentAPI {
 	public static final String NLP_WORDSEG = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_wordseg";
 	//语音合成-AI LAB
 	public static final String AAI_TTS = "https://api.ai.qq.com/fcgi-bin/aai/aai_tts";
+	//语音识别-echo版
+	public static final String AAI_ASR = "https://api.ai.qq.com/fcgi-bin/aai/aai_asr";
 }
