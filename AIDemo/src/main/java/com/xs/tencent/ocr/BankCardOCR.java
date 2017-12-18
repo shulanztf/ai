56f03131618f91f6c420b5ef01cc622951e5c230
小帅丶 <578274480@qq.com>
2017-12-18 18:10:18 +0800

    :sparkles:增肌银行卡识别 行驶证识别Java-API方式调用示例代码

---
+++ AIDemo/src/main/java/com/xs/tencent/ocr/BankCardOCR.java
@@ -0,0 +1,46 @@
+package com.xs.tencent.ocr;
+
+import java.util.HashMap;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import com.xs.tencent.HttpsUtil4Tencent;
+import com.xs.tencent.TencentAPI;
+import com.xs.tencent.sign.Base64Util;
+import com.xs.tencent.sign.TencentAISign;
+import com.xs.tencent.sign.TencentAISignSort;
+import com.xs.util.baidu.FileUtil;
+
+
+
+/**
+ * @author 小帅丶
+ * @类名称  BankCardOCR
+ * @remark 
+ * @date  2017-12-18
+ */
+public class BankCardOCR {
+	public static void main(String[] args) throws Exception {
+		//时间戳
+		String time_stamp = System.currentTimeMillis()/1000+"";
+		//图片的二进制数组数据
+		byte [] imageData = FileUtil.readFileByBytes("G:/bank.jpg");
+		//图片的base64编码数据
+		String image = Base64Util.encode(imageData);
+		//随机字符串
+		String nonce_str = TencentAISign.getRandomString(10);
+		HashMap<String, String> headers = new HashMap<String, String>();
+		HashMap<String, String> bodys = new HashMap<String, String>();
+		headers.put("Content-Type", "application/x-www-form-urlencoded");
+		bodys.put("app_id","自己的appid");
+		bodys.put("time_stamp",time_stamp);
+		bodys.put("nonce_str", nonce_str);
+		bodys.put("image", image);
+		//计算SIGN
+		String sign = TencentAISignSort.getSignature(bodys);
+		bodys.put("sign", sign);
+		HttpResponse responseBD = HttpsUtil4Tencent.doPostTencentAI(TencentAPI.OCR_BANK,headers,bodys);
+		System.out.println(EntityUtils.toString(responseBD.getEntity()));
+	}
+}
