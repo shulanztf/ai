9bbcdef6dcf64df5e7c50dcbc5fd2541941f8976
小帅丶 <578274480@qq.com>
2017-12-18 18:09:32 +0800

    :sparkles:增加银行卡识别 行驶证识别接口地址

--- AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
+++ AIDemo/src/main/java/com/xs/tencent/TencentAPI.java
@@ -4,7 +4,7 @@ public class TencentAPI {
 	//自己的APPID
 	public static final Integer APP_ID_AI = 123456;
 	//自己的APPKEY
-	public static final String APP_KEY_AI = "自己的key";
+	public static final String APP_KEY_AI = "自己key";
 	/**
 	 * 人脸检测接口地址
 	 */
@@ -26,4 +26,8 @@ public class TencentAPI {
 	public static final String AAI_TTS = "https://api.ai.qq.com/fcgi-bin/aai/aai_tts";
 	//语音识别-echo版
 	public static final String AAI_ASR = "https://api.ai.qq.com/fcgi-bin/aai/aai_asr";
+	//银行卡识别
+	public static final String OCR_BANK = "https://api.ai.qq.com/fcgi-bin/ocr/ocr_creditcardocr";
+	//行驶证驾驶证OCR识别
+	public static final String OCR_DRIVERLICENSEOCR="https://api.ai.qq.com/fcgi-bin/ocr/ocr_driverlicenseocr";
 }
