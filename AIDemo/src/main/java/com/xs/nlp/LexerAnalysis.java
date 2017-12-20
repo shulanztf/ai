69f335954a958b79687ee94068c3cd4249281423
小帅丶 <578274480@qq.com>
2017-12-20 09:22:04 +0800

    :sparkles:新增词法分析示例代码

--- AIDemo/src/main/java/com/xs/nlp/LexerAnalysis.java
+++ AIDemo/src/main/java/com/xs/nlp/LexerAnalysis.java
@@ -1,30 +1,31 @@
 package com.xs.nlp;
 
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
-
-import com.xiaoleilu.hutool.lang.Base64;
+import java.util.List;
+import com.alibaba.fastjson.JSON;
 import com.xs.common.APIContants;
+import com.xs.pojo.nlp.LexerAnalysisBean;
+import com.xs.pojo.nlp.LexerAnalysisBean.Items;
 import com.xs.util.baidu.HttpUtil;
-import com.xs.util.baidu.HttpUtils;
-import com.xs.util.baidu.UnicodeUtil;
 /**
  * 自然语言处理-词法分析
- * @author 小帅帅丶
+ * @author 小帅丶
  *
  */
 public class LexerAnalysis {
 	/**
-	 * 接口地址
+	 * 词法分析接口地址
 	 */
-	public String LEXERANALYSIS_URL = "https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer";
+	public static String LEXERANALYSIS_URL = "https://aip.baidubce.com/rpc/2.0/nlp/v1/lexer";
 	public static void main(String[] args) throws Exception {
-		LexerAnalysis analysis = new  LexerAnalysis();
-		System.out.println(analysis.getLexerAnalysis("百度是一家互联网公司", APIContants.NLP_TOKEN));
+		//输出字符串内容
+//		String result = getLexerAnalysisResult("百度是一家互联网公司", APIContants.NLP_TOKEN);
+//		System.out.println(result);
+		LexerAnalysisBean lexerAnalysisBean = getLexerAnalysisBean("百度是一家互联网公司", APIContants.NLP_TOKEN);
+		List<Items> items = lexerAnalysisBean.getItems();
+		for (int i = 0; i < items.size(); i++) {
+			//输出Items对象中basic_words内容
+			System.out.println(items.get(i).getBasic_words());
+		}
 	}
 	/**
 	 * 词法分析接口
@@ -33,12 +34,26 @@ public class LexerAnalysis {
 	 * @return data
 	 * @throws Exception
 	 */
-	public String getLexerAnalysis(String text,String accessToken) throws Exception {
+	public static String getLexerAnalysisResult(String text,String accessToken) throws Exception {
 		String url_param = "?access_token="+accessToken;
 		String url = LEXERANALYSIS_URL+url_param;	
-		HttpUtil httpUtil = new HttpUtil();
 		String param = "{\"text\":\""+text+"\"}";
-		String data = httpUtil.postNLP(url, param);
+		String data = HttpUtil.postNLP(url, param);
 		return data;
+	}
+	/**
+	 * 词法分析接口
+	 * @param text
+	 * @param accessToken
+	 * @return data
+	 * @throws Exception
+	 */
+	public static LexerAnalysisBean getLexerAnalysisBean(String text,String accessToken) throws Exception {
+		String url_param = "?access_token="+accessToken;
+		String url = LEXERANALYSIS_URL+url_param;	
+		String param = "{\"text\":\""+text+"\"}";
+		String data = HttpUtil.postNLP(url, param);
+		LexerAnalysisBean lexerAnalysisBean = JSON.parseObject(data,LexerAnalysisBean.class);
+		return lexerAnalysisBean;
 	}
 }
