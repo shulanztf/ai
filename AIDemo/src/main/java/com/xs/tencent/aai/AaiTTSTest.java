04cadefa62d9d93a2d9d29cb2917234c92413c3a
小帅丶 <578274480@qq.com>
2017-12-28 08:54:29 +0800

    :checkered_flag:修复语音合成腾讯AILab参数问题

--- AIDemo/src/main/java/com/xs/tencent/aai/AaiTTSTest.java
+++ AIDemo/src/main/java/com/xs/tencent/aai/AaiTTSTest.java
@@ -1,23 +1,23 @@
 package com.xs.tencent.aai;
 
-import java.net.URLEncoder;
 import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
 
 import com.xs.tencent.HttpsUtil4Tencent;
 import com.xs.tencent.TencentAPI;
 import com.xs.tencent.sign.TencentAISign;
 import com.xs.tencent.sign.TencentAISignSort;
-
+/**
+ * 腾讯AI语音合成 AILab接口示例代码
+ * @author 小帅丶
+ * @update 2017年12月28日 
+ */
 public class AaiTTSTest {
 	public static void main(String[] args) throws Exception {
 		//时间戳
 		String time_stamp = System.currentTimeMillis()/1000+"";
 		//合成的文本信息
-		String text = "你好";
+		String text = "你好小帅开发者";
 		//随机字符串
 		String nonce_str = TencentAISign.getRandomString(10);
 		HashMap<String, String> bodys = new HashMap<String, String>();
@@ -29,15 +29,20 @@ public class AaiTTSTest {
 		bodys.put("volume","0");
 		bodys.put("speed","100");
 		bodys.put("text", text);
+		bodys.put("aht","0");
+		bodys.put("apc", "58");
 		//计算SIGN
 		String sign = TencentAISignSort.getSignature(bodys);
 		bodys.put("sign", sign);
 		//拼接参数
 		String param = TencentAISignSort.getParams(bodys);
+		param = param.substring(0, param.length()-1);
+		System.out.println(param);
 		//接口返回数据
 		String result = HttpsUtil4Tencent.getforVoice(TencentAPI.AAI_TTS, param);
+		System.out.println(result);
 		//返回保存的语音文件路径
-		String voicePath = HttpsUtil4Tencent.getMP3Voice(result);
-		System.out.println(voicePath);
+//		String voicePath = HttpsUtil4Tencent.getMP3Voice(result);
+//		System.out.println(voicePath);
 	}
 }
