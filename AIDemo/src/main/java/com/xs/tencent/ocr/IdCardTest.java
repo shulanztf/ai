22436b935aeaf16528eaffc8a7729aef9420fd33
小帅丶 <578274480@qq.com>
2018-01-08 14:29:53 +0800

    :sparkles:修改IDCARD识别 使用新的计算SIGN方法

--- AIDemo/src/main/java/com/xs/tencent/ocr/IdCardTest.java
+++ AIDemo/src/main/java/com/xs/tencent/ocr/IdCardTest.java
@@ -4,9 +4,11 @@
 package com.xs.tencent.ocr;
 
 import java.net.URLEncoder;
+import java.util.HashMap;
 
 import com.xs.tencent.TencentAPI;
 import com.xs.tencent.sign.TencentAISign;
+import com.xs.tencent.sign.TencentAISignSort;
 import com.xs.util.baidu.Base64Util;
 import com.xs.util.baidu.FileUtil;
 import com.xs.util.baidu.HttpUtil;
@@ -30,9 +32,22 @@ public class IdCardTest {
 		//身份证图片类型，0-正面，1-反面
 		Integer card_type = 0;
 		//计算SIGN
-	   String sign =TencentAISign.appSignAI4IDCard(TencentAPI.APP_ID_AI, nonce_str, image,time_stamp,card_type);
-	   String params ="app_id="+TencentAPI.APP_ID_AI.toString()+"&time_stamp="+time_stamp+"&nonce_str="+nonce_str+"&sign="+sign+"&image="+URLEncoder.encode(image,"UTF-8")+"&card_type=0";
-	   String result = HttpUtil.post(TencentAPI.OCR_IDCARD, params);
-	   System.out.println(result);
+//	   String sign =TencentAISign.appSignAI4IDCard(TencentAPI.APP_ID_AI, nonce_str, image,time_stamp,card_type);
+//	   String params ="app_id="+TencentAPI.APP_ID_AI.toString()+"&time_stamp="+time_stamp+"&nonce_str="+nonce_str+"&sign="+sign+"&image="+URLEncoder.encode(image,"UTF-8")+"&card_type=0";
+	   //新方法计算SIGN
+		HashMap<String, String> bodys = new HashMap<String, String>();
+		bodys.put("app_id","自己的appid");
+		bodys.put("time_stamp",time_stamp);
+		bodys.put("nonce_str", nonce_str);
+		bodys.put("image", image);
+		bodys.put("card_type", "0");
+		//计算SIGN
+		String sign = TencentAISignSort.getSignature(bodys);
+		//添加SIGN参数到bodys
+		bodys.put("sign", sign);
+		//拼接接口所需要的参数的字符串 a=1&b=3&c=5......
+		String params = TencentAISignSort.getParams(bodys);
+	    String result = HttpUtil.post(TencentAPI.OCR_IDCARD, params);
+	    System.out.println(result);
 	}
 }
