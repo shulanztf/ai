2f4d02e480ccf5bed230f785286c362108cb8659
小帅丶 <578274480@qq.com>
2018-03-08 17:13:24 +0800

    :sparkles:修改为新版鉴权调用

--- AIDemo/src/main/java/com/xs/tencent/ptu/FaceCosmeticTest.java
+++ AIDemo/src/main/java/com/xs/tencent/ptu/FaceCosmeticTest.java
@@ -11,8 +11,10 @@ import org.apache.http.util.EntityUtils;
 import com.xs.tencent.HttpsUtil4Tencent;
 import com.xs.tencent.TencentAPI;
 import com.xs.tencent.sign.TencentAISign;
+import com.xs.tencent.sign.TencentAISignSort;
 import com.xs.util.baidu.Base64Util;
 import com.xs.util.baidu.FileUtil;
+import com.xs.util.baidu.HttpUtil;
 
 /**
  * 人脸美妆
@@ -20,7 +22,7 @@ import com.xs.util.baidu.FileUtil;
  *
  */
 public class FaceCosmeticTest {
-public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		// 时间戳
 		String time_stamp = System.currentTimeMillis() / 1000 + "";
 		// 图片的二进制数组数据
@@ -29,18 +31,17 @@ public static void main(String[] args) throws Exception {
 		String img64 = Base64Util.encode(imageData);
 		// 随机字符串
 		String nonce_str = TencentAISign.getRandomString(10);
-		String sign = TencentAISign.appSignBaseAI4FaceCosmetic(TencentAPI.APP_ID_AI, nonce_str, img64, 22);
-		Map<String, String> headers = new HashMap<String, String>();
-		Map<String, String> bodys = new HashMap<String, String>();
-		headers.put("Content-Type", "application/x-www-form-urlencoded");
-		bodys.put("app_id","1106471787");
-		bodys.put("time_stamp",time_stamp);
-		bodys.put("nonce_str", nonce_str);
-		bodys.put("sign", sign);
-		bodys.put("image", img64);
-		bodys.put("cosmetic", "22");
-		HttpResponse responseBD = HttpsUtil4Tencent.doPostTencentAI(TencentAPI.FACE_COSMETIC, headers, bodys);
-		System.out.println(EntityUtils.toString(responseBD.getEntity()));
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("app_id", TencentAPI.APP_ID_AI.toString());
+		params.put("time_stamp", time_stamp);
+		params.put("nonce_str", nonce_str);
+		params.put("image", img64);
+		params.put("cosmetic", "1");
+		String sign = TencentAISignSort.getSignature(params);
+		params.put("sign", sign);
+		String param = TencentAISignSort.getParams(params);
+		String result = HttpUtil.post(TencentAPI.FACE_COSMETIC, param);
+		System.out.println(result);
 	}
 
 }
